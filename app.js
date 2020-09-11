c85a9dbec7121db875532f240e256761dfa17f34
King LEE <a0988698776@gmail.com>
2020-09-11 14:50:49 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose"); // 載入 mongoose
+mongoose.connect("mongodb://localhost/restaurant-list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection;
+// 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+// 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 const app = express();
 
