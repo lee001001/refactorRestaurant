1f211021626ce3fd6af7388e421b9255e503424e
King LEE <a0988698776@gmail.com>
2020-10-13 20:19:24 +0800

    feat: add  login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,4 +6,14 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+// receive the value from login router
+router.post('/login', (req, res) => {
+
+})
+
+// create a register router modulre
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 module.exports = router
