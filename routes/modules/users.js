d0195fd6a8a8947f874c1c8f210355859ec51a99
King LEE <a0988698776@gmail.com>
2020-11-05 10:58:11 +0800

    feat: add user registration feature & refactor: add button in the register page and login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 //  create a login router module
 router.get('/login', (req, res) => {
@@ -14,6 +15,27 @@ router.post('/login', (req, res) => {
 // create a register router modulre
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  // 接收註冊表單的參數
+  const { name, email, password, confirmPassword } = req.body
+  // 在資料庫找到使用者的email
+  User.findOne({ email }).then(user => {
+    // 如果找到則回傳已經註冊過, 且回註冊頁面
+    if (user) {
+      console.log('The User already register')
+      res.render('register')
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
 })
 
 module.exports = router
