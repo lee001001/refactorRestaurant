fd8e7c82626d3ef09ffd3a1370b03a8caaa10638
King LEE <a0988698776@gmail.com>
2020-11-07 23:09:50 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,21 +1,46 @@
-const mongoose = require('mongoose')
-const Restaurant = require('../restaurant') // 載入 Restaurant model Scheam
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const bcrypt = require('bcryptjs')
+
+const Restaurant = require('../restaurant')
 const restaurantList = require('../restaurant.json').results
 
-mongoose.connect('mongodb://localhost/restaurant-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const User = require('../user')
+const db = require('../../config/mongoose')
+const SEED_USERS = [{
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678'
+},
+{
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678'
+}]
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  for (let i = 0; i < restaurantList.length; i++) {
-    Restaurant.create({ ...restaurantList[i] }) // creat seeds arrays
-  }
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USERS[0].password, salt))
+    .then(hash => {
+      SEED_USERS.forEach(SEED_USER => SEED_USER.password = hash)
+      return User.insertMany(SEED_USERS)
+    })
+    .then(users => {
+      return Promise.all(restaurantList.map((restaurants, index) => {
+        if (index < 3) {
+          restaurants.userId = users[0]._id
+          return Restaurant.create(Object.assign({}, restaurants))
+        }
+        if (index < 6) {
+          restaurants.userId = users[1]._id
+          return Restaurant.create(Object.assign({}, restaurants))
+        }
+      }))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
