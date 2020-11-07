427cf43520970fc7d3f522f2e3811376da648e9c
King LEE <a0988698776@gmail.com>
2020-11-07 13:02:26 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
-//  const Restaurant = require('./models/restaurant')
 const port = 3000
 app.use(express.static('public'))
 
@@ -26,11 +26,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use(flash())
 
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -22,6 +22,21 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   // 接收註冊表單的參數
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      confirmPassword
+    })
+  }
   // 在資料庫找到使用者的email
   User.findOne({ email }).then(user => {
     // 如果找到則回傳已經註冊過, 且回註冊頁面
@@ -43,6 +58,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
